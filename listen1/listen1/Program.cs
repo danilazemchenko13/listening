37962c427f99260f8ae073edf96d8b042ad3cd5c
Данила Земченко <ZemchenkoDM@stud.kai.ru>
2021-01-16 14:06:35 +0300

    создал проект

---
+++ listen1/listen1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace listen1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
